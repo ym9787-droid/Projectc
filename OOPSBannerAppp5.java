ea783ef6a617639305d77700fd75cb82bd3853c9
ym9787-droid <ym9787@srmist.edu.in>
2026-03-11 13:43:56 +0530

    Add files via upload

---
+++ OOPSBannerAppp5.java
@@ -0,0 +1,10 @@
+class KmToMiles { 
+    static void main(String[] args) { 
+        double kilometers = 10.8; 
+        double conversionFactor = 1.6; 
+ 
+        double miles = kilometers * conversionFactor; 
+ 
+        System.out.println("The distance " + kilometers + " km in miles is " + miles); 
+    } 
+} 
