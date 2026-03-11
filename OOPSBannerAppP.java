615cef030048d540cb3eefb0fd3b7b76659684ba
ym9787-droid <ym9787@srmist.edu.in>
2026-03-11 12:53:52 +0530

    Add files via upload

---
+++ OOPSBannerAppP.java
@@ -0,0 +1,10 @@
+class HarryAge { 
+     static void main(String[] args) { 
+        int birthYear = 2000; 
+        int currentYear = 2024; 
+ 
+        int age = currentYear - birthYear; 
+ 
+        System.out.println("Harry's age in " + currentYear + " is " + age); 
+    } 
+} 
